bd0574ff2da34134882ab80c7a2150411e99c441
Rob Brunett <rb3@Robs-Air.attlocal.net>
2021-05-03 20:41:28 -0400

    finished html format -needs ids- added psuedo code for js

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+/*Pseudo Code:
+1. Get current date with moment.js
+2. Enable user-input search functionality with on-click event.
+3. Capture search history in local storage
+4. Render search history to individual search buttons from local storage.
+5. Get API weather data for current day.
+    3A. Create an object to contain the data (city, temp, humidity, windspeed, UV index, and weather Icon).
+6. Get API weather data for five day forecast.
+    4A. Create an object to contain the data (city, temp, humidity, windspeed, UV index, and weather Icon).
+7. Render current day forecast data from object to primary box.
+8. Render 5 day forecast data to individual cards.*/
