218b882582abf768327ae6d9d51f6d8ab516bbf4
Rob Brunett <rb3@Robs-Air.attlocal.net>
2021-05-05 21:20:45 -0400

    primary API fetch working, but icon fetch is not

--- assets/script.js
+++ assets/script.js
@@ -10,28 +10,42 @@
 7. Render current day forecast data from object to primary box.
 8. Render 5 day forecast data to individual cards.*/
 
-var today = moment().format("L");
-var locale = $('#searchField').val().trim();
-var key = '213af9b7d597899a5c6f22af5ed57261'
-var qUrl = `https://api.openweathermap.org/data/2.5/weather?q=${locale}&appid=${key}`;
-var icon = wResponse.weather[0].icon;
-var iUrl = `https://openweathermap.org/img/w/${icon}.png`
+var date = moment().format("L");
+
+var key = '957c1d427eb08dc32b2d83caeea47227'
+
+
+
 
 //Current Weather function//
 
 function curWeather(locale) {
-    $.ajax({
-        url: qUrl,
-        method: 'GET'
-    }).then(function(wResponse) {
-
+    var qUrl = `https://api.openweathermap.org/data/2.5/weather?q=${locale}&units=imperial&appid=957c1d427eb08dc32b2d83caeea47227`;
+   
+   
+    fetch(qUrl)
+    .then(function(response) {
+        return response.json();
     })
-}
+    .then(function (data){
+        var cityData = $(`
+                    <h3> ${data.name} ${date} <img src="${iUrl}" alt="${data.weather[0].description}"</h3>
+                    <p> Current Temp: ${data.main.temp} Farenheit </p>
+                    <p> Wind Speed:  ${data.wind.speed} mph </p>
+                    <p> Humidity: ${data.main.humidity} \% </p>`);
+        var icon = data.weather[0].icon;
+        console.log('icon', icon);
+        var iUrl = `https://openweathermap.org/img/wn/${icon}.png`
+        $("#todayWeather").append(cityData);
+    })      
+    
+};
 
 
 //Search button functionality
 $('#button').on('click', function(event) {
     event.preventDefault();
-
+    var locale = $('#searchField').val().trim();
+    curWeather(locale);
 
 })
